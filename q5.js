c85453598e0eb73035a8c800d6d6757518c0cf89
zjhch123 <416836508@qq.com>
2018-12-05 19:50:04 +0800

    q5

---
+++ q5.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  let substring = ''
+  let maxLength = 0
+
+  s = '-' + s.split('').join('-') + '-'
+
+  for (let i = 0; i < s.length; i++) {
+    let count = 1
+    let left = i
+    let right = i
+    while (left - 1 >= 0 && right + 1 < s.length && s[left - 1] == s[right + 1]) {
+      left --
+      right ++
+      count += 2
+    }
+
+    if (count > maxLength) {
+      substring = s.slice(left, right)
+      maxLength = count
+    }
+  }
+
+  return substring.replace(/-/g, '')
+};
+
+console.log(longestPalindrome('e1see2ees'))

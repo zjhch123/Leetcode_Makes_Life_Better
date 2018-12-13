8ad598cbfc17b07325aafa643fcb4c96d94b8daa
zjhch123 <416836508@qq.com>
2018-12-13 21:29:45 +0800

    q17

--- q17.js
+++ q17.js
@@ -2,6 +2,14 @@
  * @param {string} digits
  * @return {string[]}
  */
+var letterCombinations = function(digits) {
+  if (digits.length == 0) {
+    return []
+  }
+  
+  return combine(digits)
+};
+
 const keys = {
   '0': ' ',
   '1': '*',
@@ -14,14 +22,6 @@ const keys = {
   '8': 'tuv',
   '9': 'wxyz'
 }
-
-var letterCombinations = function(digits) {
-  if (digits.length == 0) {
-    return ''
-  }
-  
-  return combine(digits)
-};
 
 var combine = function(digits) {
   if (digits.length == 1) {
@@ -39,5 +39,3 @@ var combine = function(digits) {
 
   return ret
 }
-
-console.log(letterCombinations('1234567'))

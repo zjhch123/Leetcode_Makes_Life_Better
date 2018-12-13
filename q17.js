1e5497d63ddf64c18ff5f5d297d66294020bae76
zjhch123 <416836508@qq.com>
2018-12-13 20:35:39 +0800

    q17

---
+++ q17.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const keys = {
+  '0': ' ',
+  '1': '*',
+  '2': 'abc',
+  '3': 'def',
+  '4': 'ghi',
+  '5': 'jkl',
+  '6': 'mno',
+  '7': 'pqrs',
+  '8': 'tuv',
+  '9': 'wxyz'
+}
+
+var letterCombinations = function(digits) {
+  if (digits.length == 0) {
+    return ''
+  }
+  
+  return combine(digits)
+};
+
+var combine = function(digits) {
+  if (digits.length == 1) {
+    return keys[digits].split('')
+  }
+  const ret = []
+  const chrs = keys[digits[0]].split('')
+  const prev = combine(digits.slice(1))
+
+  for (let i = 0; i < chrs.length; i++) {
+    for(let j = 0; j < prev.length; j++) {
+      ret.push(chrs[i] + prev[j])
+    }
+  }
+
+  return ret
+}
+
+console.log(letterCombinations('1234567'))

a4a50838f14b0c8b3550b50a95f7c9397b84811b
zjhch123 <416836508@qq.com>
2019-01-06 19:43:28 +0800

    q39

---
+++ q39.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+  if (candidates.length === 0) {
+    return []
+  }
+  
+  let result = []
+
+  function dfs(candidates, tempArr, remain, start) {
+    if (remain < 0) {
+      return
+    } else if (remain === 0) {
+      result.push(tempArr.slice(0))
+      return
+    } else {
+      for (let i = start; i < candidates.length; i++) {
+        tempArr.push(candidates[i])
+        dfs(candidates, tempArr, remain - candidates[i], i)
+        tempArr.pop()
+      }
+    }
+  }
+
+  dfs(candidates, [], target, 0)
+
+  return result
+};

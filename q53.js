65e06618d0a33b7352aa6ca9062a1f0c9534c292
zjhch123 <416836508@qq.com>
2019-02-16 21:20:08 +0800

    q53

--- q53.js
+++ q53.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-  
+  let max = nums[0]
+  let nowMax = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    nowMax = Math.max(nums[i], nums[i] + nowMax)
+    max = Math.max(nowMax, max)
+  }
+
+  return max
 };

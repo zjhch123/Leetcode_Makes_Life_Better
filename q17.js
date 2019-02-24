bdc687db6a44df50118f168ef067832eb6403789
zjhch123 <416836508@qq.com>
2019-02-24 20:46:07 +0800

    q17

--- q17.js
+++ q17.js
@@ -3,39 +3,41 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
+  const keys = {
+    '0': ' ',
+    '1': '',
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  }
+
   if (digits.length == 0) {
     return []
   }
-  
-  return combine(digits)
-};
 
-const keys = {
-  '0': ' ',
-  '1': '*',
-  '2': 'abc',
-  '3': 'def',
-  '4': 'ghi',
-  '5': 'jkl',
-  '6': 'mno',
-  '7': 'pqrs',
-  '8': 'tuv',
-  '9': 'wxyz'
-}
-
-var combine = function(digits) {
-  if (digits.length == 1) {
-    return keys[digits].split('')
-  }
   const ret = []
-  const chrs = keys[digits[0]].split('')
-  const prev = combine(digits.slice(1))
 
-  for (let i = 0; i < chrs.length; i++) {
-    for(let j = 0; j < prev.length; j++) {
-      ret.push(chrs[i] + prev[j])
+  const combine = (digits, str = [], index = 0) => {
+    if (index === digits.length) {
+      ret.push(str.join(''))
+      return
+    }
+
+    const digit = digits[index]
+    for (let i = 0; i < keys[digit].length; i++) {
+      str.push(keys[digit][i])
+      combine(digits, str, index + 1)
+      str.length = str.length - 1
     }
   }
 
+  combine(digits)
   return ret
-}
+};
+
+console.log(letterCombinations('234'))

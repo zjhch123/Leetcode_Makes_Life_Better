400ca0879019aee441cc5648c570ff59baa51ebf
zjhch123 <416836508@qq.com>
2019-03-03 16:34:23 +0800

    q84

--- q84.js
+++ q84.js
@@ -34,3 +34,52 @@ var largestRectangleArea = function(heights) {
 
   return helper(heights, 0, heights.length - 1)
 };
+
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea2 = function(heights) {
+  if (heights === null || heights.length === 0) {
+    return 0
+  }
+  const stack = []
+  
+  let index = 0
+  let max = 0
+
+  while (index < heights.length) { // 构造单调递增栈, 顺便计算最大值
+    const height = heights[index]
+    if (stack.length === 0) {
+      stack.push(height)
+    } else {
+      const peek = stack[stack.length - 1]
+      if (peek < height) {
+        stack.push(height)
+      } else {
+        let count = 0
+        while (stack[stack.length - 1] > height) {
+          count += 1
+          const outer = stack.pop()
+          max = Math.max(outer * count, max)
+        }
+        while (count >= 0) {
+          stack.push(height)
+          count -= 1
+        }
+      }
+    }
+    index += 1
+  }
+
+  const size = stack.length
+  for (let i = 0; i < stack.length; i++) {
+    max = Math.max(stack[i] * (size - i), max)
+  }
+
+  return max
+};
+
+console.log(largestRectangleArea([4,1,6,8,3,6,5]))
+console.log(largestRectangleArea2([4,1,6,8,3,6,5]))

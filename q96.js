9473663e034d62b78ca9ae251be2b3ba319d88f6
zjhch123 <416836508@qq.com>
2019-03-20 17:07:18 +0800

    q96

---
+++ q96.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+  const cached = new Map()
+  const dfs = (n) => {
+    if (n <= 1) {
+      return 1
+    }
+    if (cached.has(n)) {
+      return cached.get(n)
+    }
+    let ret = 0
+    for (let i = 1; i <= n; i++) {
+      ret += dfs(i - 1) * dfs(n - i)
+    }
+
+    cached.set(n, ret)
+    return ret
+  }
+
+  return dfs(n)
+};
+
+console.log(numTrees(0))

5b6acb76dead6ef90254c946e876ec0d9a8174b6
zjhch123 <416836508@qq.com>
2019-04-03 17:22:59 +0800

    q107

---
+++ q107.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+  if (root === null) {
+    return []
+  }
+
+  const ret = []
+  const queue = [root]
+
+  while (queue.length > 0) {
+    const numbers = []
+    let count = queue.length
+
+    while (count > 0) {
+      count -= 1
+      
+      const node = queue.shift()
+      if (node === null) {
+        continue
+      }
+
+      queue.push(node.left, node.right)
+      numbers.push(node.val)
+    }
+
+    if (numbers.length > 0) {
+      ret.unshift(numbers)
+    }
+  }
+
+  return ret
+};

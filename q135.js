c0d1960a776b6c94084255b85d4635076bb26414
zjhch123 <416836508@qq.com>
2019-05-06 15:21:38 +0800

    q135

---
+++ q135.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function(ratings) {
+  const candies = Array(ratings.length).fill(1)
+
+  for (let i = 1; i < ratings.length; i++) {
+    if (ratings[i] > ratings[i - 1]) {
+      candies[i] = candies[i - 1] + 1
+    }
+  }
+
+
+  for (let i = ratings.length - 2; i >= 0; i--) {
+    if (ratings[i] > ratings[i + 1]) {
+      candies[i] = Math.max(candies[i], candies[i + 1] + 1)
+    }
+  }
+
+  return candies.reduce((a, b) => a + b, 0)
+};
+
+console.log(candy([1,2,87,87,87,2,1]))

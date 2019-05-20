8b534ad5f7c576c83a6216a16ee5ea11f6ec681d
zjhch123 <416836508@qq.com>
2019-05-20 21:36:02 +0800

    q141

---
+++ q141.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  if (head === null || head.next === null) {
+    return false
+  }
+
+  let ptr = head
+  let nPtr = head.next
+
+  while (nPtr !== null && nPtr.next !== null) {
+    if (ptr === nPtr) {
+      return true
+    }
+    ptr = ptr.next
+    nPtr = nPtr.next.next
+  }
+
+  return false
+};

2ed633ec385e4dda318659b5642bfa4627234a1a
zjhch123 <416836508@qq.com>
2019-07-18 21:00:08 +0800

    q153

--- q153.js
+++ q153.js
@@ -13,14 +13,24 @@ var findMin = function(nums) {
     return nums[0]
   }
 
-  while (right - left > 1) {
+  while (left < right) {
+    while (left < right && nums[left] === nums[left + 1]) {
+      left += 1
+    }
+
     const mid = ~~((left + right) / 2)
-    if (nums[left] > nums[mid]) {
-      right = mid
+
+    if (nums[left] <= nums[mid] && nums[right] < nums[mid]) {
+      left = mid + 1
     } else {
-      left = mid
+      right = mid
     }
   }
 
-  return nums[right]
+  return nums[left]
 };
+
+console.log(findMin([2,2,2,2,2,3,3,3,3,3,0,2,2,2,2,2,2]))
+
+
+// 2 2 1 2 2 2
